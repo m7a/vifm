6d645690c18fb735a28c588e7ccc8f8cbea8cbac
xaizek <xaizek@openmailbox.org>
2016-09-27 21:03:16 +0300

    Make bg_execute() able to create joinable threads

--- src/trash.c
+++ src/trash.c
@@ -287,7 +287,7 @@ empty_trash_dir(const char trash_dir[])
 	char *const trash_dir_copy = strdup(trash_dir);
 
 	if(bg_execute(task_desc, op_desc, BG_UNDEFINED_TOTAL, 1, &empty_trash_in_bg,
-			trash_dir_copy) != 0)
+			trash_dir_copy, NULL) != 0)
 	{
 		free(trash_dir_copy);
 	}
